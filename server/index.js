157d131481e594bdc39d15da887f429a187e89da
Syeda Farhin <79400522+SyedaFarhin@users.noreply.github.com>
2026-07-15 21:49:24 +0530

    Update CORS origin for production deployment

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://websitebuilder-frontend-pxc5.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
